6f5b5a72a6d53b1f53763c1a12703622d0db66a2
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-10-22 09:37:22 -0500

    New if statement. Needs to be fixed so submit correctly
    Please debug @wanhack. Needs to submit only when submit button is clicked.

--- web-app.py
+++ web-app.py
@@ -19,10 +19,12 @@ if(st.button('Seal')):
 elif(st.button('Retrieve')):
     URL=st.text_input('URL')
     Date=st.date_input('Date')
-    url2 = f"https://web.archive.org/web/{Date}100101/{URL}" 
-    page = requests.get(url2)        # pulls the url as a request.
-    soup = BeautifulSoup(page.text, "html.parser")      # uses beautiful soup to parse the "url" for only the html.parser
-    htmlfile = open("page.html", "w", encoding="utf-8")     # openning the output file // this is not gonna be the final version. Needs to be changed to output 
-    stringed_page = str(soup)       # turns the soup object into a string to use for the writing to a file.
-    htmlfile.write(stringed_page)       # writes the string to the file. 
-    print("Flag hits")
+    if(st.button('Submit')):
+        url2 = f"https://web.archive.org/web/{Date}100101/{URL}" 
+        page = requests.get(url2)        # pulls the url as a request.
+        soup = BeautifulSoup(page.text, "html.parser")      # uses beautiful soup to parse the "url" for only the html.parser
+        htmlfile = open("page.html", "w", encoding="utf-8")     # openning the output file // this is not gonna be the final version. Needs to be changed to output 
+        stringed_page = str(soup)       # turns the soup object into a string to use for the writing to a file.
+        htmlfile.write(stringed_page)       # writes the string to the file. 
+        print("Flag hits")
+    
