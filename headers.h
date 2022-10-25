1d25430fbc3beefb64dae8dd4311c0249cadd6ab
Bailey Shelton <104048251+bcshelto565@users.noreply.github.com>
2022-10-25 00:06:35 -0500

    Delete headers.h

--- headers.h
+++
@@ -1,4 +0,0 @@
-int check_directory_exits(const char *path);
-char* directory(int year,char *event_name,char * data);
-void create_file(char* dir_name,char* event_name,char * data);
-char* read_event(int year, char* event_name)
